252116a00a1be83ed21f287698f29704f2f9ac8d
Pedro Henrique de Freitas Diniz Neto <71715984+pedrodiniz29@users.noreply.github.com>
2025-08-09 20:13:46 +0000

    atualiza api status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,22 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+  const postgresVersion = await database.query("SHOW server_version;");
+  const dbActiveConnections = await database.query(
+    "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname='postgres';",
+  );
+  const dbMaxConnections = await database.query("SHOW max_connections;");
+
   response.status(200).json({
-    response: "Deu certo",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        postgres_version: postgresVersion.rows[0].server_version,
+        db_active_connections: dbActiveConnections.rows[0].count,
+        db_max_connections: parseInt(dbMaxConnections.rows[0].max_connections),
+      },
+    },
   });
 }
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,13 @@
 test("Get api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  console.log(responseBody);
+  expect(response.status).toBe(200); //Verifica status
+
+  expect(responseBody.updated_at).toBeDefined(); // Verifica se tem a propriedade
+  expect(responseBody.dependencies).toBeDefined(); // Verifica se tem a propriedade
+  expect(responseBody.dependencies.database).toBeDefined(); // Verifica se tem a propriedade
+  expect(responseBody.dependencies.database.postgres_version).toBe("16.0"); // Verifica se tem a propriedade
+  expect(responseBody.dependencies.database.db_active_connections).toBe(1); // Verifica se tem a propriedade
 });
