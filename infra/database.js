564e06c69e6f566422ae199be5a78d5114fd4843
Pedro Henrique de Freitas Diniz Neto <71715984+pedrodiniz29@users.noreply.github.com>
2025-08-09 13:54:53 +0000

    Modificacao scripts e variaveis de ambiente

---
+++ infra/database.js
@@ -0,0 +1,21 @@
+import { Client } from "pg";
+
+async function query(queryObject) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  await client.connect();
+
+  const res = await client.query(queryObject);
+
+  await client.end();
+  return res;
+}
+
+export default {
+  query: query,
+};
