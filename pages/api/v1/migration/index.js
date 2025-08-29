7ce5a94497231d196d4f942f8563d37b9364d105
Pedro Henrique de Freitas Diniz Neto <71715984+pedrodiniz29@users.noreply.github.com>
2025-08-29 19:41:14 +0000

    Corrige migrations endpoint

--- pages/api/v1/migration/index.js
+++ pages/api/v1/migration/index.js
@@ -3,7 +3,49 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migration(request, response) {
-  const dbClient = await database.getNewClient();
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+    const migrationsObject = {
+      dbClient: dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
+
+    const method = request.method;
+
+    console.log(method);
+    switch (method) {
+      case "POST":
+        console.log("Entrou no POST");
+        const migrations_post = await migrationRunner({
+          ...migrationsObject,
+          dryRun: false,
+        });
+        if (migrations_post.length > 0) {
+          return response.status(201).json(migrations_post);
+        }
+        return response.status(200).json(migrations_post);
+
+      case "GET":
+        console.log("Entrou no GET");
+        const migrations_get = await migrationRunner(migrationsObject);
+        return response.status(200).json(migrations_get);
+
+      default:
+        console.log("Entrou no DEFAULT");
+        return response.status(405).end();
+    }
+  } catch (error) {
+    console.log(error);
+    throw error;
+  } finally {
+    await dbClient.end();
+  }
 
   const migrationsObject = {
     dbClient: dbClient,
