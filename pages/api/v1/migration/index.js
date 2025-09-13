cda18c5d914fd7b6d9722f4d335c099a9aafba42
Pedro Henrique de Freitas Diniz Neto <71715984+pedrodiniz29@users.noreply.github.com>
2025-09-13 19:59:05 +0000

    remove codigo api migrations

--- pages/api/v1/migration/index.js
+++ pages/api/v1/migration/index.js
@@ -46,40 +46,4 @@ export default async function migration(request, response) {
   } finally {
     await dbClient.end();
   }
-
-  const migrationsObject = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
-
-  const method = request.method;
-
-  console.log(method);
-  switch (method) {
-    case "POST":
-      console.log("Entrou no POST");
-      const migrations_post = await migrationRunner({
-        ...migrationsObject,
-        dryRun: false,
-      });
-      await dbClient.end();
-      if (migrations_post.length > 0) {
-        return response.status(201).json(migrations_post);
-      }
-      return response.status(200).json(migrations_post);
-
-    case "GET":
-      console.log("Entrou no GET");
-      const migrations_get = await migrationRunner(migrationsObject);
-      await dbClient.end();
-      return response.status(200).json(migrations_get);
-
-    default:
-      console.log("Entrou no DEFAULT");
-      return response.status(405).end();
-  }
 }
