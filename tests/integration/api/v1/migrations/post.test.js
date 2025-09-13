4390e55bb8a5bfa4f657e4e8fdc6b6ad2a4daaeb
Pedro Henrique de Freitas Diniz Neto <71715984+pedrodiniz29@users.noreply.github.com>
2025-09-13 21:52:28 +0000

    makes npm test more robust with async retry e orchestrador

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,10 +1,10 @@
 import database from "infra/database.js";
+import orchestrador from "tests/orchestrador.js";
 
-beforeAll(cleanDatabase);
-
-async function cleanDatabase() {
+beforeAll(async () => {
+  await orchestrador.waitForAllServices();
   await database.query("drop schema public cascade; create schema public;");
-}
+});
 
 test("Post api/v1/migration should return 201", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migration", {
